45a5bb8befc75906143c207649606c122cb13e61
wu8888712-png <wu8888712@gmail.com>
2026-05-12 17:30:19 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "var(--border)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
-};
+  plugins: [],
+}
